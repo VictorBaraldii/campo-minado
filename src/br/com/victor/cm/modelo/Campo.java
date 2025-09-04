6c74d586e873e3897d96d9c92aa4f80f73eca482
exercio-java-poo <victorbaraldi@gmail.com>
2025-09-03 21:14:48 -0300

    Enviando tarefa de hoje

--- src/br/com/victor/cm/modelo/Campo.java
+++ src/br/com/victor/cm/modelo/Campo.java
@@ -1,5 +1,7 @@
 package br.com.victor.cm.modelo;
 
+import br.com.victor.cm.excecao.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +23,7 @@ public class Campo {
 
     boolean adicionarVizinho(Campo vizinho) {
         boolean linhaDiferente = linha != vizinho.linha;
-        boolean colunaDiferente = linha != vizinho.coluna;
+        boolean colunaDiferente = coluna != vizinho.coluna;
         boolean diagonal = linhaDiferente && colunaDiferente;
 
         int deltaLinha = Math.abs(linha - vizinho.linha);
@@ -37,5 +39,50 @@ public class Campo {
         } else {
             return false;
         }
+    }
+
+    void alternarMarcacao() {
+        if(!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+    private boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+    void minar() {
+        minado = true;
+    }
+
+    public boolean isMarcado() {
+        return marcado;
+    }
+
+    public boolean isAberto() {
+        return aberto;
+    }
+
+    public boolean isFechado() {
+        return !isAberto();
     }
 }
