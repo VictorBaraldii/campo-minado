d30cac7a9aab5bccd89616e1cd39e58cdf0ed09f
exercio-java-poo <victorbaraldi@gmail.com>
2025-09-06 10:08:02 -0300

    Enviando implementação visão

--- br/com/victor/cm/Aplicacao.java
+++ br/com/victor/cm/Aplicacao.java
@@ -1,18 +1,14 @@
 package br.com.victor.cm;
 
 import br.com.victor.cm.modelo.Tabuleiro;
+import br.com.victor.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
     public static void main(String[] args) {
 
-        Tabuleiro tabuileiro = new Tabuleiro(6,6,6);
+        Tabuleiro tabuleiro = new Tabuleiro(6,6,6);
+        new TabuleiroConsole((tabuleiro));
 
-        tabuileiro.abrir(3, 3);
-        tabuileiro.alternarMarcacao(4,4);
-        tabuileiro.alternarMarcacao(4,5);
-
-
-        System.out.println(tabuileiro);
     }
 }
---
+++ br/com/victor/cm/excecao/SairException.java
@@ -0,0 +1,6 @@
+package br.com.victor.cm.excecao;
+
+public class SairException extends RuntimeException  {
+
+    private static final long serialVersionUID = 1;
+}
