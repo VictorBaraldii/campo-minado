f7d519312e0ee03badeeec875d5167b2a8d0a8df
exercio-java-poo <victorbaraldi@gmail.com>
2025-09-06 10:44:07 -0300

    Implementação final do Projeto

--- br/com/victor/cm/modelo/Tabuleiro.java
+++ br/com/victor/cm/modelo/Tabuleiro.java
@@ -1,5 +1,7 @@
 package br.com.victor.cm.modelo;
 
+import br.com.victor.cm.excecao.ExplosaoException;
+
 import java.math.MathContext;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,10 +26,16 @@ public class Tabuleiro {
     }
 
     public void abrir(int linha, int coluna) {
-        campos.parallelStream()
-                .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-                .findFirst()
-                .ifPresent(c -> c.abrir());
+        try {
+            campos.parallelStream()
+                    .filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
+                    .findFirst()
+                    .ifPresent(c -> c.abrir());
+        } catch (ExplosaoException e) {
+            campos.forEach(c -> c.setAberto(true));
+            throw e;
+
+        }
 
     }
 
@@ -61,9 +69,9 @@ public class Tabuleiro {
         Predicate<Campo> minado = c -> c.isMinado();
 
         do {
-            minasArmadas = campos.stream().filter(minado).count();
             int aleatorio = (int) (Math.random() * campos.size());
             campos.get(aleatorio).minar();
+            minasArmadas = campos.stream().filter(minado).count();
         } while(minasArmadas < minas);
     }
 
@@ -79,9 +87,20 @@ public class Tabuleiro {
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
+        sb.append("  ");
+        for (int c = 0; c < colunas; c++) {
+            sb.append(" ");
+            sb.append(c);
+            sb.append(" ");
+
+        }
+
+        sb.append("\n");
+        
         int i = 0;
         for (int l = 0; l < linhas; l++) {
-
+            sb.append(l);
+            sb.append(" ");
             for (int c = 0; c < colunas; c++) {
                 sb.append(" ");
                 sb.append(campos.get(i));
